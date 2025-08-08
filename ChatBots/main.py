3a3f775b3e3da4b3bffadfbbb89ca3eb7791089d
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2025-08-08 20:42:19 +0200

    Add files via upload

---
+++ ChatBots/main.py
@@ -0,0 +1,23 @@
+from services.perchance import PerchanceChatBot
+
+if __name__ == "__main__":
+    Perchance = PerchanceChatBot() # automatic message handling
+
+    while True:
+        user_input = input("Chat prompt: ")
+        # Handle commands?!
+        message_lower = user_input.lower()
+        if message_lower == "clear":
+            Perchance.clear()
+            print("Cleared messages.")
+            continue
+        if "remove" in message_lower:
+            amount = message_lower.replace("remove", "")
+            try:
+                Perchance.MessagesHandler.remove(int(amount.strip()))
+            except Exception as e:
+                print("...", e)
+                continue
+            print("Removed 1.")
+            continue
+        print("[Perchance]", Perchance.generate(user_input.strip())) # if its empty just try again ig
