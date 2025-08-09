a0e1e5f7c98a0f4184f9b21ab83ec565fc30452e
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2025-08-09 02:38:41 +0200

    Update main.py

--- ImageGenerators/main.py
+++ ImageGenerators/main.py
@@ -1,10 +1,13 @@
 from services.vheer import Vheer
 from services.writecream import WriteCream
+from services.perchance import PerchanceImageGenerator
 
 if __name__ == "__main__":
     vheer = Vheer()
     writecream = WriteCream()
+    perchance = PerchanceImageGenerator()
 
     user_input = input("Image prompt: ")
     print("[vheer]", vheer.generate(user_input.strip()))
     print("[writecream]", writecream.generate(user_input.strip()))
+    print("[perchance]", perchance.generate(user_input.strip()))
